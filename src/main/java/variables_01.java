fc003babb7d4d918c1e046b1de3a81054c1c1185
Shivani <vyas2590@gmail.com>
2025-07-29 15:50:57 +0200

    first commit

---
+++ src/main/java/variables_01.java
@@ -0,0 +1,94 @@
+public class variables_01 {
+
+
+//single line comment :made with(//)-non functional for JAVA
+//psvm or main
+
+        public static void main(String[]args){
+
+            int a =5;
+            int b= 6;
+
+            System.out.println(a);
+
+            System.out.println(b);
+
+            System.out.println(a+b);
+
+//HEY,THIS IS A COMMENT!:)
+/*
+
+psvm/main:To create the main method
+
+To create a variable,you first need the data type,
+Then a name, and a value.
+Usage:{datatype} {name}={value};  (* value( name or letter ) with string data type always be written in double quote)
+-String firstName="John"
+-int age=15
+
+Toprintanyvalue:sout
+Usage:System.out.println({value})
+-System.out.println("Hey");
+-System.out.println(15);
+-System.out.println(age);
+
+
+Do not forget to put a semicolon(;)at the end:)
+
+*/
+
+
+//singlelinecomment:Example1:Create a variable called'hi',and put"Hello"text inside of it
+
+            String hi="Hello";
+            System.out.println(hi);
+
+            System.out.println("Hii!");
+
+//Example2:Create a variable called'age',and put any number inside of it,then print it.
+
+//comment:belowtwolinescodeisdynamiccoding
+            String age="50";
+            System.out.println(age);
+
+/*multiline comment:to print something directly,sout-System.out.println("xyz");-put word,alphabet,numbers
+In double coat in bracket when string data type is used.
+*/
+
+//comment:below line is called Hardcoding
+//in general practice-->dynamic coding is used,for example in social media app development.
+
+            System.out.println("35");
+
+//Example3:Createavariablecalled'usernamepassword',andputanynumberorvalueinsideofit,thenprintit.
+
+            String username="xyz123";
+            String password="xyz@123";
+
+            System.out.println(username);
+            System.out.println(password);
+
+//AdditionalExamples:
+
+
+//exampleofstring
+            System.out.println("Hey");
+
+//exampleofinteger
+            System.out.println(15);
+
+//exampleofintegersum
+            System.out.println(2+3);
+
+//exampleofstring
+            System.out.println("Number:"+3);
+
+//exampleofstring
+            System.out.println("5"+3);
+
+
+
+        }
+    }
+
+
